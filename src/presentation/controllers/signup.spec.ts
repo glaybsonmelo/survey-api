272d2c941214558599a36f4e5eb9ae443a96676b
Glaybsbon <gleybsonmelo998@gmail.com>
2023-06-26 10:55:14 -0300

    refactor: move sut creation to a factory helper methot

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,6 +1,10 @@
 import { SignUpController } from './signup';
 import { MissinParamError } from '../erros/missing-param-error';
 
+const makeSur = (): SignUpController => {
+  return new SignUpController();
+};
+
 describe('signUp Controller', () => {
   it('Should return 400 if no name is provided', () => {
     const sut = new SignUpController();
