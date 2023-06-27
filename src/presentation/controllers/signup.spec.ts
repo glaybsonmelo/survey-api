7e75703b00a6819994f375cdcfbcd6b3b3fe4718
Glaybsbon <gleybsonmelo998@gmail.com>
2023-06-27 14:55:12 -0300

    test: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -2,6 +2,7 @@ import { SignUpController } from './signup';
 import { MissinParamError } from '../erros/missing-param-error';
 import { InvalidParamError } from '../erros/invalid-param-error';
 import { EmailValidator } from '../protocols/email-validator';
+import { ServerError } from '../erros/server-error';
 
 interface SutTypes {
   sut: SignUpController;
@@ -110,5 +111,27 @@ describe('signUp Controller', () => {
     };
     sut.handle(httpRequest);
     expect(isValidSpy).toHaveBeenCalledWith('any_email@email.com');
+  });
+
+  it('Should return 500 if EmailValidator throws', () => {
+    class EmailValidatorStub implements EmailValidator {
+      isValid(email: string): boolean {
+        throw new Error();
+      }
+    }
+    const emailValidatorStub: EmailValidator = new EmailValidatorStub();
+    const sut = new SignUpController(emailValidatorStub);
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
   });
 });
